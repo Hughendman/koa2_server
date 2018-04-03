f25d184a0ca33144fabc0b2bb9349bb9bbbdff12
yinxs <xuesong_yin@126.com>
2018-04-03 14:46:45 +0800

    query

--- routes/test2.js
+++ routes/test2.js
@@ -4,6 +4,7 @@ const json_1 = require('../public/json/test1');
 router.prefix('/test_async')
 
 router.get('/yinxs', function (ctx, next) {
+    let query = ctx.query;
     let time = null ;
     async function test1(a) {
         let b = a.join(',');
@@ -17,6 +18,7 @@ router.get('/yinxs', function (ctx, next) {
         times = test1_2(b);
         json_1.past = time;
         json_1.pasts = times;
+        json_1.query = query;
         ctx.body = json_1;
     }
     let arr = ['I am test1','I am test2','I am test3'];
